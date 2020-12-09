0362596770488144ed0cd25bf2a8380cefb2faef
Karn <mail@karngyan.com>
2020-12-09 14:07:37 +0530

    ad crossorigin

--- src/main/java/com/karngyan/newsletter/controller/SubscriberController.java
+++ src/main/java/com/karngyan/newsletter/controller/SubscriberController.java
@@ -13,15 +13,13 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Duration;
 
 @RestController
 @Slf4j
+@CrossOrigin
 @RequestMapping(SubscriberController.NEWSLETTER_API_ENDPOINT)
 public class SubscriberController {
 
